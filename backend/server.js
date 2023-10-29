3eda690d71832c6802fb344e9aa31b905d6e91cb
MATHU <henrymathu74@gmail.com>
2023-10-29 11:37:24 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const app = require('express')()
+const http = require('http').Server(app)
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+require('dotenv').config()
+require('./db')
+
+app.use(cors({origin:true,credentials:true}))
+app.use(bodyparser.json())
+
+
+app.listen(process.env.PORT,console.log(`server is running ${process.env.PORT}`))
